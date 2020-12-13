22e949adca7d37778dd4670fd2f53840de187c05
Michael Dylan Ah Teck <ahteck@usc.edu>
2020-12-13 18:23:52 +0800

    implement service calls in comparison-table

--- CryptoDashboard/src/app/comparison-table/comparison-table.component.ts
+++ CryptoDashboard/src/app/comparison-table/comparison-table.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CryptodetailsService } from '../details/crytodetails.service'
+import { Component, OnInit, Injectable, Input } from '@angular/core';
+import { CompareService } from './compare.service'
 import { PriceBySource } from '../Interfaces/PriceBySource'
 @Component({
   selector: 'app-comparison-table',
@@ -8,9 +8,51 @@ import { PriceBySource } from '../Interfaces/PriceBySource'
 })
 export class ComparisonTableComponent implements OnInit {
 
-  constructor(private cryptodetailsService: CryptodetailsService) { }
   tableData: [PriceBySource]
+  constructor(private compareService: CompareService) { }
+
+  @Input() symbol: string
+
   ngOnInit(): void {
+
+    this.retrieveTableData()
   }
 
+  retrieveTableData() {
+
+    this.addBTCData()
+    this.addBinanceData()
+    //For new exchanges, just add here
+    //E.g addCrytoTradesData()
+  }
+
+  addBTCData() {
+    this.compareService.getBTCLatestPrice(this.symbol).subscribe(
+      (value) => {
+        const row: PriceBySource = {
+          source: 'Btcmarket',
+          sourceUrl: 'https://www.btcmarkets.net/',
+          price: value.lastPrice,
+          link: `https://app.btcmarkets.net/buy-sell?market=${value.marketId}`
+        }
+        this.tableData.push(row)
+      }
+    )
+  }
+
+  addBinanceData() {
+    this.compareService.getBinanceLatestPrice(this.symbol).subscribe(
+      (value) => {
+        const row: PriceBySource = {
+          source: 'Binance',
+          sourceUrl: 'https://www.binance.com/',
+          price: value.lastPrice,
+          priceChange: value.priceChange,
+          pricePercentageChange: value.priceChangePercent,
+          link: `https://www.binance.com/en/trade/${value.symbol}`
+        }
+        this.tableData.push(row)
+      }
+    )
+  }
 }
